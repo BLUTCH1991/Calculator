30fdd64a7b44fac22392f5d05b1b93c5d8102de5
Thomas Chabot <thomas.chabot@hotmail.fr>
2018-01-25 14:53:03 +0100

    Add javadoc to Gradle

--- src/main/java/com/imie/calculator/Addition.java
+++ src/main/java/com/imie/calculator/Addition.java
@@ -3,17 +3,28 @@ package com.imie.calculator;
 public class Addition {
 
 	protected int value = 0;
-	
+
+	/**
+	 *
+	 * @param a
+	 */
 	public Addition(int a) {
 		this.value = a;
 	}
-	
+
+	/**
+	 *
+	 * @param b
+	 */
 	public void make(int b) {
 		this.value += b;
 	}
-	
+
+	/*
+	 *
+	 */
 	public int getValue() {
 		return this.value;
 	}
-	
+
 }
--- src/main/java/com/imie/calculator/Divide.java
+++ src/main/java/com/imie/calculator/Divide.java
@@ -2,20 +2,35 @@ package com.imie.calculator;
 
 public class Divide {
 
+	/**
+	 *
+	 */
 	protected int value = 0;
-	
+
+	/**
+	 *
+	 * @param a
+	 */
 	public Divide(int a) {
 		this.value = a;
 	}
-	
+
+	/**
+	 *
+	 * @param b
+	 */
 	public void make(int b) {
 		if (b == 0) {
 			this.value = Integer.MIN_VALUE;
 		}else {
-			this.value /= b;			
+			this.value /= b;
 		}
 	}
-	
+
+	/**
+	 *
+	 * @return
+	 */
 	public int getValue() {
 		return this.value;
 	}
