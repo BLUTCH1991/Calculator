6dd5487288a13109149945dd6bbeb1a4b79ede14
Thomas Chabot <thomas.chabot@hotmail.fr>
2018-01-25 09:24:23 +0100

    Fix divide test

--- src/test/java/com/imie/calculator/DivideTest.java
+++ src/test/java/com/imie/calculator/DivideTest.java
@@ -7,16 +7,16 @@ import org.junit.Test;
 
 public class DivideTest {
 	
-	private Divide div = new Divide(2);
+	private Divide div;
 
 	@Before
 	public void setUp() throws Exception {
+		this.div = new Divide(10);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		org.junit.Assert.assertNotNull(this.div);
-		org.junit.Assert.assertEquals(20,this.div.value);
+		this.div = null;
 	}
 
 	@Test
@@ -24,16 +24,16 @@ public class DivideTest {
 		this.div.make(2);
 		
 		org.junit.Assert.assertNotNull(this.div);
-		org.junit.Assert.assertEquals(1,this.div.getValue());
+		org.junit.Assert.assertEquals(5,this.div.getValue());
 		
 		this.div.make(0);
 		Assert.assertEquals(Integer.MIN_VALUE, this.div.value);
 	}
 	
-	/*@Test
+	@Test
 	public void testGetValue() {
 		org.junit.Assert.assertNotNull(this.div);
-		org.junit.Assert.assertEquals(1,this.div.getValue());
-	}*/
+		org.junit.Assert.assertEquals(10,this.div.getValue());
+	}
 
 }
